b82349f5050d893bb4e8e81a476eb60a3d1d0fe0
Djent- <inflames2361@aim.com>
2015-12-21 15:06:05 -0500

    Added goals for the program.

--- WallDatabase.go
+++ WallDatabase.go
@@ -11,3 +11,17 @@ import (
 func main() {
   
 }
+
+/*
+Basic flow:
+  User starts program
+  Program hosts web server on :8080
+  Serve HTML page displaying current wallpaper image and checkbox list of tags
+    also a text form for new tags
+    Bonus points for not using javascript
+  User selects tags
+  Tags are POSTed back to web server listener
+  Filename, tags are added to database
+  Web server starts serving next wallpaper image HTML
+  Web page auto-refreshes
+*/
