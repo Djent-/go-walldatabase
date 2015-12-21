196577f0807f4c3e66450a967daad15b8a52a39f
Djent- <inflames2361@aim.com>
2015-12-21 15:08:26 -0500

    Add SQLite imports

--- WallDatabase.go
+++ WallDatabase.go
@@ -6,6 +6,8 @@ import (
   "fmt"
   "io/ioutil"
   "net/http"
+  "database/sql"
+  _ "github.com/mattn/go-sqlite3"
 )
 
 func main() {
@@ -18,6 +20,7 @@ Basic flow:
   Program hosts web server on :8080
   Serve HTML page displaying current wallpaper image and checkbox list of tags
     also a text form for new tags
+    For wallpapers which are already tagged, some checkboxes should default to true
     Bonus points for not using javascript
   User selects tags
   Tags are POSTed back to web server listener
