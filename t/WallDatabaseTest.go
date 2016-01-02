ec64ffd46ec3c85131a22d14007d9dd0f0f70d2c
PH <djent.git@cock.li>
2016-01-02 11:23:02 -0500

    Added more tests

--- t/WallDatabaseTest.go
+++ t/WallDatabaseTest.go
@@ -2,9 +2,19 @@ package main
 
 import (
 	"fmt"
-	_ "github.com/djent-/go-walldatabase"
+	wdb "github.com/djent-/go-walldatabase"
+	//"strings"
 )
 
 func main() {
 	fmt.Println("Testing WallDatabase.go")
+	testwp := wdb.NewWP("test.jpg", []string{"stars", "night", "snow", "trees"})
+	fmt.Println(testwp)
+	testdb := wdb.OpenDB("go-walls.db")
+	err := testdb.Add(testwp)
+	if err != nil { fmt.Println(err) }
+	testget := testdb.Get("stars")
+	for _, wp := range(testget) {
+		fmt.Println(wp)
+	}
 }
